9f4f71d6aa9caa43591e885cc3adc981d500c3bd
MuhammadAbdullah6969 <134133623+MuhammadAbdullah6969@users.noreply.github.com>
2024-06-22 14:56:49 +0500

    tries databases11

--- icoder/wsgi.py
+++ icoder/wsgi.py
@@ -8,8 +8,8 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'icoder.settings')
 
