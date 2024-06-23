f1ce3bed62895dcea287183172e768ad4eabb0cf
MuhammadAbdullah6969 <134133623+MuhammadAbdullah6969@users.noreply.github.com>
2024-06-23 15:23:36 +0500

    Configure for Vercel2 deployment

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,6 @@
-#!/bin/bash
-
 # Use Python and pip provided by Vercel environment
-export PATH="/usr/local/bin:$PATH"
-python3 -m pip install --upgrade pip
+python -m pip install --upgrade pip
 pip install -r requirements.txt
 
 # Collect static files
-python3 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
