353f6b24b35bfaf8e4871866b9ac58c4325d4b2f
MuhammadAbdullah6969 <134133623+MuhammadAbdullah6969@users.noreply.github.com>
2024-06-23 12:20:11 +0500

    vercel

--- icoder/settings.py
+++ icoder/settings.py
@@ -91,12 +91,27 @@ WSGI_APPLICATION = 'icoder.wsgi.application'
     }
 } """
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': ':memory:',
     }
-}
+} """
+
+if os.getenv('VERCEL'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
