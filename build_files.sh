a724a06499f95476971e629222ca7f668ca013d7
MuhammadAbdullah6969 <134133623+MuhammadAbdullah6969@users.noreply.github.com>
2024-06-23 15:36:22 +0500

    Configure for Vercel5 deployment

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,2 @@
-# Install SQLite dependencies
-apt-get update
-apt-get install -y libsqlite3-dev
-
-# Use Python and pip provided by Vercel environment
-python3 -m pip install --upgrade pip
 pip3 install -r requirements.txt
-
-# Collect static files
 python3 manage.py collectstatic --noinput
