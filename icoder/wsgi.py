91761d9615787bc60c28b3c190166c78adf09fa5
MuhammadAbdullah6969 <134133623+MuhammadAbdullah6969@users.noreply.github.com>
2024-06-23 16:01:22 +0500

    Configure for Vercel8 deployment

--- icoder/wsgi.py
+++ icoder/wsgi.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
+from whitenoise.middleware import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'icoder.settings')
 
 application = get_wsgi_application()
-
-app = application
+application = WhiteNoise(application)
