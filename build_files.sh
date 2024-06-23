e2df8be849622608c85f1a15c30a2253fc58ee17
MuhammadAbdullah6969 <134133623+MuhammadAbdullah6969@users.noreply.github.com>
2024-06-23 11:58:42 +0500

    uploading again1

--- build_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Collecting static files"
-python3 -m pip install -r requirements.txt --noinput 
-python3 manage.py makemigrations --noinput 
-python3 manage.py migrate --noinput 
-python3 manage.py collectstatic --noinput --clear
