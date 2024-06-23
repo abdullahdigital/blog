ab538e4bb230474f7f81067df955a206a6078779
MuhammadAbdullah6969 <134133623+MuhammadAbdullah6969@users.noreply.github.com>
2024-06-23 12:03:02 +0500

    uploading again1

--- icoder/settings.py
+++ icoder/settings.py
@@ -87,9 +87,10 @@ WSGI_APPLICATION = 'icoder.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-} 
+}
+
 
 
 # Password validation
