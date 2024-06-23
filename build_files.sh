6f54cfca2c72dd79532627a4737263b2d497b8cd
MuhammadAbdullah6969 <134133623+MuhammadAbdullah6969@users.noreply.github.com>
2024-06-23 11:35:22 +0500

    tries databases17

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "Collecting static files"
+python3 -m pip install -r requirements.txt --noinput 
+python3 manage.py makemigrations --noinput 
+python3 manage.py migrate --noinput 
 python3 manage.py collectstatic --noinput --clear
