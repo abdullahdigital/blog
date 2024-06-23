1f0fea2edfdd3c8b8454a440c4a9e29c05e4626e
MuhammadAbdullah6969 <134133623+MuhammadAbdullah6969@users.noreply.github.com>
2024-06-23 15:57:42 +0500

    Configure for Vercel7 deployment

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,12 @@
-pip3 install -r requirements.txt
+#pip3 install -r requirements.txt
+#python3 manage.py collectstatic --noinput
+# Use Python and pip provided by Vercel environment
+export PATH="/usr/local/bin:$PATH"
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+
+# Apply Django migrations
+python3 manage.py migrate
+
+# Collect static files
 python3 manage.py collectstatic --noinput
